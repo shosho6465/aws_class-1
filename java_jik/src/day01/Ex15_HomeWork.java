f0f757cf41b0eb058c9a00d8b80df9ed862929d1
st8324 <stajun@naver.com>
2025-12-01 18:09:26 +0900

    자바 01일차 : 과제 - 학점 switch로 출력

---
+++ java_jik/src/day01/Ex15_HomeWork.java
@@ -0,0 +1,18 @@
+package day01;
+
+public class Ex15_HomeWork {
+
+	public static void main(String[] args) {
+		
+		/* 성적을 입력받아 성적에 맞는 학점을 출력하는 코드를 작성하세요. switch
+		 * A : 90 ~ 109
+		 * B : 80 ~ 89
+		 * C : 70 ~ 79
+		 * D : 60 ~ 69
+		 * F : -9 ~ 59
+		 * 잘못된 성적 : -9미만, 110이상 
+		 * */
+
+	}
+
+}
