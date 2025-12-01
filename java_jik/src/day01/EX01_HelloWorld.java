5c6a23c04321c119e49933a8cdc080f739147ca8
st8324 <stajun@naver.com>
2025-12-01 12:21:38 +0900

    자바 01일차 : 기본 예제

---
+++ java_jik/src/day01/EX01_HelloWorld.java
@@ -0,0 +1,14 @@
+package day01;
+
+/* 실행 방법 : Run 메뉴 > Run 클릭(단축키 : Ctrl + F11)
+ * 주석 단축키 : 한줄 주석(Ctrl + /) 여러줄 주석(블록 지정 후 Ctrl + Shift + /)
+*/
+public class EX01_HelloWorld {
+
+	public static void main(String []args) {
+		/* 표준 출력을 이용하여 콘솔에 문자열을 출력
+		 * */
+		System.out.println("Hello World");
+	}
+
+}
