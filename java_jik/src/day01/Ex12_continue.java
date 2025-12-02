5bca4ca8769d91f55b3cbffda7d21393a4c464e7
Jeongwon-me <kangaroo_you@naver.com>
2025-12-02 19:59:16 +0900

    자바

---
+++ java_jik/src/day01/Ex12_continue.java
@@ -0,0 +1,23 @@
+package day01;
+
+public class Ex12_continue {
+
+	public static void main(String[] args) {
+		/* continue
+		 * -반복문에서 continue를 만나면 지정 위치로 이동
+		 * -지정 위치
+		 *   -for문 : 증감식
+		 *   -while, do while문 : 조건식
+		 * -if문과 함께 사용
+		 *
+		 */
+		int num = 7;
+		//1부터 10 사이의 짝수를 출력 예제를 continue를 이용 => 홀수이면 스킵
+		for(int i = 1; i <= num; i++) {
+			if( i % 2 != 0) {
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+}
